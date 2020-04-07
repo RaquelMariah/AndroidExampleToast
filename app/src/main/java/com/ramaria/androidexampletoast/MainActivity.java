c00d2da1520a171ace605c171a18d736494008d2
Raquel Maria <45218570+RaquelMariah@users.noreply.github.com>
2020-04-07 13:39:26 -0300

    Adicionado toast

--- app/src/main/java/com/ramaria/androidexampletoast/MainActivity.java
+++ app/src/main/java/com/ramaria/androidexampletoast/MainActivity.java
@@ -2,7 +2,11 @@ package com.ramaria.androidexampletoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void exibirToast(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "Hello Toast!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+
     }
 }
